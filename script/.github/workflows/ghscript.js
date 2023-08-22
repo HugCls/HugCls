e6c7139d12cd6ed73c3562894b832ea19829eec9
HugCls <superhugh92@gmail.com>
2023-08-22 16:38:51 +0200

    structure ready

--- script/.github/workflows/ghscript.js
+++ script/.github/workflows/ghscript.js
@@ -3,7 +3,11 @@ import { readFileSync, writeFileSync } from 'fs';
 
 // Obtenir la date actuelle
 const date = new Date();
-const dateString = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+const month = String(date.getMonth() + 1).padStart(2, '0');
+const day = String(date.getDate()).padStart(2, '0');
+const year = date.getFullYear();
+
+const dateString = `${year}-${month}-${day}`;
 
 // Obtenir une citation inspirante
 get('https://zenquotes.io/api/random')
